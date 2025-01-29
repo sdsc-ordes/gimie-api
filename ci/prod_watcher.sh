0144c14794902a32500c68f6cdaf7ba2f6529285
Carlos Vivar Rios <45425937+caviri@users.noreply.github.com>
2025-01-29 15:52:30 +0100

    ci: prod_watcher

---
+++ ci/prod_watcher.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# chmod +x prod_watcher.sh
+# Usage: */5 * * * * /imaging-plaza/ci/prod_watcher.sh >> /imaging-plaza/ci/prod_watcher.log 2>&1
+
+cd /imaging-plaza/gimie-api || exit
+
+# Fetch the latest changes from the remote
+git fetch origin main > /dev/null 2>&1
+
+# Check if local is behind remote
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/main)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+  echo "$(date '+%Y-%m-%d %H:%M:%S'). Changes detected. Pulling..."
+  git pull origin main
+  
+  # Stop and remove the old container
+  docker stop gimie-api || true
+  docker rm gimie-api || true
+  
+  # Rebuild and run the new container
+  docker build  -t gimie-api .
+  docker run -d --name gimie-api --env-file .env -p 7000:15400 gimie-api
+else
+  echo "$(date '+%Y-%m-%d %H:%M:%S'). No changes found."
+fi
