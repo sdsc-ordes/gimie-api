8f7d78ecb5f3300fa030be5c38fafd42c127fea7
cmdoret <cyril.mattheydoret@gmail.com>
2025-03-07 17:57:00 +0100

    feat: add nix devshell

---
+++ tools/nix/flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "gimie-api";
+
+  nixConfig = {
+    extra-substituters = [
+      # Nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-23.11";
+    # Also see the 'stable-packages' overlay at 'overlays/default.nix'.
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      # The function which builds the flake output attrMap.
+      defineOutput =
+        system:
+        let
+          # inherit from nixpkgs
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          # Things needed only at compile-time.
+          packagesBasic = with pkgs; [
+            age
+            bash
+            coreutils
+            curl
+            findutils
+            git
+            imgpkg
+            just
+            kubectl
+            sops
+            vendir
+            yamlfmt
+            ytt
+            zsh
+          ];
+
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              packages = packagesBasic;
+            };
+
+            ci = pkgs.mkShell {
+              packages = packagesBasic;
+
+              # Due to some weird handling of TMPDIR inside containers:
+              # https://github.com/NixOS/nix/issues/8355
+              # We have to reset the TMPDIR to make `nix build` work inside
+              # a development shell.
+              # Without `nix develop` it works.
+              shellHook = "unset TMPDIR";
+            };
+          };
+        };
+    in
+    # Creates an attribute map `{ <key>.<system>.default = ...}`
+    # by calling function `defineOutput`.
+    # Key sofar is only `devShells` but can be any output `key` for a flake.
+    flake-utils.lib.eachDefaultSystem defineOutput;
+}
